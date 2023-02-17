101514b3545bbe3f4ff150689bbd017479072ca0
sfeng <sh492064@dal.ca>
2023-02-17 17:36:39 -0400

    Refactor and add comments

--- app/src/main/java/com/example/group17project/HomepageActivity.java
+++ app/src/main/java/com/example/group17project/HomepageActivity.java
@@ -19,7 +19,6 @@ import java.util.Objects;
 public class HomepageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
   private DrawerLayout drawerLayout;
-  private NavigationView navigationView;
   private ActionBarDrawerToggle toggle;
   private FragmentManager fragmentManager;
 
@@ -28,19 +27,25 @@ public class HomepageActivity extends AppCompatActivity implements NavigationVie
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_homepage);
 
+    // drawer layout instance to toggle the menu icon to open
+    // drawer and back button to close drawer
     drawerLayout = findViewById(R.id.drawer_layout);
     toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+    // pass the Open and Close toggle for the drawer layout listener
+    // toggle the button
     drawerLayout.addDrawerListener(toggle);
     toggle.syncState();
+    // make the Navigation drawer icon always appear on the action bar
     Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-    navigationView = findViewById(R.id.navigation_view);
-
+    NavigationView navigationView = findViewById(R.id.navigation_view);
+    // Set this activity as the listener for item selections in the NavigationView
     navigationView.setNavigationItemSelectedListener(this);
+
+    // be aware that this fragment manager is from package androidx
     fragmentManager = getSupportFragmentManager();
-    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    fragmentTransaction.replace(R.id.fragment_container, new ReceiverFragment());
-    fragmentTransaction.commit();
+    fragmentTransaction(new ReceiverFragment());
   }
 
   @Override
@@ -63,12 +68,21 @@ public class HomepageActivity extends AppCompatActivity implements NavigationVie
         newFragment = new ProviderFragment();
       }
 
-      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-      fragmentTransaction.replace(R.id.fragment_container, newFragment);
-      fragmentTransaction.commit();
+      fragmentTransaction(newFragment);
     }
-    
+
     drawerLayout.closeDrawer(GravityCompat.START);
     return true;
+  }
+
+  /**
+   * A helper method to swap fragments
+   *
+   * @param other the destination fragment
+   */
+  private void fragmentTransaction(Fragment other) {
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.fragment_container, other);
+    fragmentTransaction.commit();
   }
 }
