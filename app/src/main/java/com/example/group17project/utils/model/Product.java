c943ab20463d3b58e7dd777b0d62b3633ac0fefc
sfeng <sh492064@dal.ca>
2023-03-08 23:34:16 -0400

    create product model and its controller

---
+++ app/src/main/java/com/example/group17project/utils/model/Product.java
@@ -0,0 +1,126 @@
+package com.example.group17project.utils.model;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class Product {
+  private String name;
+  private String description;
+  private Date dateAvailable;
+  private Type type;
+  private String ownerID;
+  private String locationID;
+  private double price;
+  private Type preferredExchange;
+  private Status status;
+
+  public Product(String name, String ownerID) {
+    this.name = name;
+    this.ownerID = ownerID;
+
+    dateAvailable = new Date();
+    status = Status.AVAILABLE;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Date getDateAvailable() {
+    return dateAvailable;
+  }
+
+  public void setDateAvailable(Date dateAvailable) {
+    this.dateAvailable = dateAvailable;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public void setType(Type type) {
+    this.type = type;
+  }
+
+  public String getOwnerID() {
+    return ownerID;
+  }
+
+  public void setOwnerID(String ownerID) {
+    this.ownerID = ownerID;
+  }
+
+  public String getLocationID() {
+    return locationID;
+  }
+
+  public void setLocationID(String locationID) {
+    this.locationID = locationID;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public Type getPreferredExchange() {
+    return preferredExchange;
+  }
+
+  public void setPreferredExchange(Type preferredExchange) {
+    this.preferredExchange = preferredExchange;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return "Product{" +
+        "name='" + name + '\'' +
+        ", description='" + description + '\'' +
+        ", dateAvailable=" + dateAvailable +
+        ", type=" + type +
+        ", ownerID='" + ownerID + '\'' +
+        ", locationID='" + locationID + '\'' +
+        ", price=" + price +
+        ", preferredExchange=" + preferredExchange +
+        ", status=" + status +
+        '}';
+  }
+
+  public enum Type {
+    BABY_TOYS,
+    CLOTHES,
+    COMPUTER_ACCESSORIES,
+    MOBILE_PHONES,
+    FURNITURE
+  }
+
+  public enum Status {
+    AVAILABLE,
+    SOLD_OUT
+  }
+}
---
+++ app/src/main/java/com/example/group17project/utils/repository/ProductRepository.java
@@ -0,0 +1,59 @@
+package com.example.group17project.utils.repository;
+
+import com.example.group17project.utils.model.Product;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class ProductRepository {
+  private final DatabaseReference databaseRef;
+
+  public ProductRepository(FirebaseDatabase database, boolean isTest) {
+    this.databaseRef = database.getReference(isTest ? "test-products" : "products");
+  }
+
+  public ProductRepository(FirebaseDatabase database) {
+    this(database, false);
+  }
+
+  /**
+   * Creates a new product in the database.
+   *
+   * @param name    The name of the product.
+   * @param ownerID The ID of the owner of the product.
+   */
+  public void createProduct(String name, String ownerID) {
+    String key = databaseRef.push().getKey();
+    Product product = new Product(name, ownerID);
+    databaseRef.child(Objects.requireNonNull(key)).setValue(product);
+  }
+
+  public void createProduct(Product product) {
+    String key = databaseRef.push().getKey();
+    databaseRef.child(Objects.requireNonNull(key)).setValue(product);
+  }
+
+  /**
+   * Deletes a product from the database.
+   *
+   * @param id The ID of the product to delete.
+   */
+  public void deleteProduct(String id) {
+    databaseRef.child(id).removeValue();
+  }
+
+  /**
+   * Updates a product in the database.
+   *
+   * @param id      The ID of the product to update.
+   * @param product The product to update.
+   */
+  public void updateProduct(String id, Product product) {
+    databaseRef.child(id).setValue(product);
+  }
+
+  public void deleteAllProducts() {
+    databaseRef.removeValue();
+  }
+}
