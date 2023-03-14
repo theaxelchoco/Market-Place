ccb44a30d4fe00b4042eaeb7cde2fa7da87fa496
mabrar02 <mahdeen02@gmail.com>
2023-03-14 18:43:42 -0300

    Refactoring: Changing scrollview to display dates fully

--- app/src/main/java/com/example/group17project/AddProductActivity.java
+++ app/src/main/java/com/example/group17project/AddProductActivity.java
@@ -3,6 +3,8 @@ package com.example.group17project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
