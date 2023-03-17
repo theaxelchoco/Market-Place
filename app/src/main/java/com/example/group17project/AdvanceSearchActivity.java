f316ac264c281f0fb002b692633282a012dfeab9
sfeng <sh492064@dal.ca>
2023-03-16 22:03:13 -0300

    finish advance search activity design

---
+++ app/src/main/java/com/example/group17project/AdvanceSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.group17project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdvanceSearchActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_advance_search);
+  }
+}
