70be2a40162d7fe4141fd3ca2d02390b488bc5f4
mabrar02 <mahdeen02@gmail.com>
2023-03-17 19:40:39 -0300

    Created the test classes for providerlist

---
+++ app/src/androidTest/java/com/example/group17project/ProvidersListEspressoTest.java
@@ -0,0 +1,45 @@
+package com.example.group17project;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ProvidersListEspressoTest {
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.group17project", appContext.getPackageName());
+    }
+}
