7be12ebf0c940f1f81e1d9e4236098f3e7091034
mabrar02 <mahdeen02@gmail.com>
2023-03-17 23:22:24 -0300

    Created tests for receiver list, most failing currently

--- app/src/androidTest/java/com/example/group17project/ReceiverEspressoTest.java
+++ app/src/androidTest/java/com/example/group17project/ReceiverEspressoTest.java
@@ -1,43 +1,75 @@
 package com.example.group17project;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.group17project.utils.model.Product;
+import com.example.group17project.utils.model.User;
+import com.example.group17project.utils.repository.ProductRepository;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Calendar;
+import java.util.Date;
 
 @RunWith(AndroidJUnit4.class)
 public class ReceiverEspressoTest {
-    static Product product;
+
+    private ProviderFragment providerFragment;
+    @Rule
+    public ActivityTestRule<HomepageActivity> myRule = new ActivityTestRule<>(HomepageActivity.class);
+    private static ProductRepository productRepo;
+    Product product;
+
+    @BeforeClass
+    public static void setUpRepo(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://w23-csci3130-group-17-default-rtdb.firebaseio.com/");
+        productRepo = new ProductRepository(database, false);
+    }
+
     @Before
     public void setUp() throws Exception{
-        product  =  new Product();
+        Calendar cal = Calendar.getInstance();
+        cal.set(2023, 3, 14);
+        product = new Product("Test Iphone 12", "test@dal.ca", "test description", cal, "Mobile Phones", "Halifax", "Clothes", 100);
+        productRepo.createProduct(product);
+
+        Thread.sleep(3000);
+
     }
 
     @After
     public void tearDown() throws Exception{
-
+        productRepo.deleteProduct(product.getProductID());
     }
 
     @Test
@@ -45,6 +77,42 @@ public class ReceiverEspressoTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.group17project", appContext.getPackageName());
+    }
+
+    @Test
+    public void testTapOnItem(){
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.productList)).atPosition(0).perform(click());
+        onView(withId(R.id.rExpandedContactBtn)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testItemExpandedBackButton(){
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.productList)).atPosition(0).perform(click());
+        onView(withId(R.id.rExpandedBackBtn)).perform(click());
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testItemContactBtn(){
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+        onData(anything()).inAdapterView(withId(R.id.productList)).atPosition(0).perform(click());
+
+    }
+
+    @Test
+    public void ownItemAddition() throws InterruptedException {
+        User.getInstance().setUserDetails("test@dal.ca");
+        Thread.sleep(5000);
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+        ListView listView = myRule.getActivity().findViewById(R.id.productList);
+        ListAdapter adapter = listView.getAdapter();
+        for(int i = 0; i < adapter.getCount(); i++){
+            onData(anything()).inAdapterView(withId(R.id.productList)).atPosition(i).perform(click());
+            onView(withId(R.id.rExpandedProductName)).check(matches(not(withText("test@dal.ca"))));
+            onView(withId(R.id.rExpandedBackBtn)).perform(click());
+        }
     }
 
 
