8e2ddcc9e0dbf758aeb6fe3c06c8f8f8f74f724a
mabrar02 <mahdeen02@gmail.com>
2023-03-17 22:50:51 -0300

    Creating UI for receiver list expanded item

---
+++ app/src/main/java/com/example/group17project/ExpandedReceiverActivity.java
@@ -0,0 +1,25 @@
+package com.example.group17project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.group17project.utils.repository.ProductRepository;
+
+
+
+import java.util.Date;
+
+public class ExpandedReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_expanded);
+
+
+    }
+}
