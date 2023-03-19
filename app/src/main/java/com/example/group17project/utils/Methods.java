fdd955373648cabc62ad864f01d71afea4aed2ca
sfeng <sh492064@dal.ca>
2023-03-18 23:47:05 -0300

    find location using google maps api

--- app/src/main/java/com/example/group17project/utils/Methods.java
+++ app/src/main/java/com/example/group17project/utils/Methods.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AlertDialog;
 
 import com.example.group17project.utils.model.Filter;
 import com.example.group17project.utils.model.ProductType;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+import java.io.IOException;
 
 public class Methods {
+  private final static String GOOGLE_MAPS_API_KEY = "AIzaSyADgAur80t_vb3uX2PEm75aNBiUq9yFOOU";
+
   private Methods() {
   }
 
@@ -30,8 +38,27 @@ public class Methods {
     );
   }
 
-  public static boolean isSameLocation(String location1, String location2) {
-    return true;
+  public static boolean isSameLocation(String city1, String city2) {
+    if (city1.isEmpty() || city2.isEmpty()) {
+      return true;
+    }
+    GeoApiContext context = new GeoApiContext.Builder()
+        .apiKey(GOOGLE_MAPS_API_KEY)
+        .build();
+
+    try {
+      GeocodingResult[] results1 = GeocodingApi.geocode(context, city1).await();
+      GeocodingResult[] results2 = GeocodingApi.geocode(context, city2).await();
+
+      if (results1.length > 0 && results2.length > 0) {
+        String city1Formatted = results1[0].addressComponents[0].longName;
+        String city2Formatted = results2[0].addressComponents[0].longName;
+        return city1Formatted.equals(city2Formatted);
+      }
+    } catch (IOException | InterruptedException | ApiException e) {
+      e.printStackTrace();
+    }
+    return false;
   }
 
   private static Range<Integer> makeRange(String min, String max) {
