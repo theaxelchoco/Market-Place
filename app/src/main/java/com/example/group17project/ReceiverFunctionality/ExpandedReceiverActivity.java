436f1b60aad3b6fbfc1bd4531846059438205928
mabrar02 <mahdeen02@gmail.com>
2023-03-28 18:09:01 -0300

    Implementation: Basic setup for transaction activity logic

--- app/src/main/java/com/example/group17project/ReceiverFunctionality/ExpandedReceiverActivity.java
+++ app/src/main/java/com/example/group17project/ReceiverFunctionality/ExpandedReceiverActivity.java
@@ -24,6 +24,15 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
 
     private Button backBtn;
     private Button contactBtn;
+    private Button buyBtn;
+    private String productId;
+    private int price;
+    private long dateVal;
+    private String name;
+    private String type;
+    private String exchange;
+    private String desc;
+    private String location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +52,6 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
      */
     protected void setViewFromIntentExtras(){
         Date date;
-        int price;
-        long dateVal;
-        String name;
-        String type;
-        String exchange;
-        String desc;
-        String location;
-
         Intent intent = this.getIntent();
         if(intent != null){
             name = intent.getStringExtra("name");
@@ -61,6 +62,8 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
             dateVal = intent.getLongExtra("date", 0);
             date = new Date(dateVal);
             price = intent.getIntExtra("price", 0);
+            productId = intent.getStringExtra("productId");
+
 
             SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.CANADA);
             String dateString = sdf.format(date);
@@ -85,6 +88,19 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void buyBtnOnClick(View view){
+        Intent intent = new Intent(ExpandedReceiverActivity.this, TransactionActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("type", type);
+        intent.putExtra("exchange", exchange);
+        intent.putExtra("location", location);
+        intent.putExtra("desc", desc);
+        intent.putExtra("date", dateVal);
+        intent.putExtra("price", price);
+        intent.putExtra("productId", productId);
+        startActivity(intent);
+    }
+
     /**
      * On click method for contact button, currently sends user back to receiver screen but in future will open user to user
      * communication screen
@@ -95,7 +111,6 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
     }
 
 
-
     protected void findViewComponents(){
         nameView = findViewById(R.id.rExpandedProductName);
         typeView = findViewById(R.id.rExpandedType);
@@ -103,11 +118,13 @@ public class ExpandedReceiverActivity extends AppCompatActivity {
 
         backBtn = findViewById(R.id.rExpandedBackBtn);
         contactBtn = findViewById(R.id.rExpandedContactBtn);
+        buyBtn = findViewById(R.id.rExpandedBuyBtn);
 
     }
 
     protected void setOnClickMethods(){
         backBtn.setOnClickListener(this::backButtonOnClick);
         contactBtn.setOnClickListener(this::contactButtonOnClick);
+        buyBtn.setOnClickListener(this::buyBtnOnClick);
     }
 }
--- app/src/main/java/com/example/group17project/ReceiverFunctionality/TransactionActivity.java
+++ app/src/main/java/com/example/group17project/ReceiverFunctionality/TransactionActivity.java
@@ -2,18 +2,81 @@ package com.example.group17project.ReceiverFunctionality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.group17project.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class TransactionActivity extends AppCompatActivity {
+
+    EditText productNameText;
+    EditText approxValText;
+    RatingBar ratingBar;
+
+    TextView nameErrorText;
+    TextView valueErrorText;
+    TextView ratingErrorText;
+
+    Button backBtn;
+    Button confirmBtn;
+
+    private int price;
+    private long dateVal;
+    private String name;
+    private String type;
+    private String exchange;
+    private String desc;
+    private String location;
+    private String productId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
+
+        findViewComponents();
+        setOnClickMethods();
+        setViewFromIntentExtras();
     }
 
+    protected void setViewFromIntentExtras(){
+        Date date;
+        Intent intent = this.getIntent();
+        if(intent != null){
+            name = intent.getStringExtra("name");
+            type = intent.getStringExtra("type");
+            exchange = intent.getStringExtra("exchange");
+            desc = intent.getStringExtra("desc");
+            location = intent.getStringExtra("location");
+            dateVal = intent.getLongExtra("date", 0);
+            date = new Date(dateVal);
+            price = intent.getIntExtra("price", 0);
+            productId = intent.getStringExtra("productId");
+
+
+            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.CANADA);
+            String dateString = sdf.format(date);
+
+
+        }
+    }
+
+    protected void findViewComponents(){
+
+
+    }
+
+    protected void setOnClickMethods(){
+
+    }
 
     public boolean isProductNameValid(String name){
         return true;
