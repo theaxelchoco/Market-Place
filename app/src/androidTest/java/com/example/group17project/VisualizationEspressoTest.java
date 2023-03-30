b07e7e45e230609cc808efccfe67c98abed14acf
Qixuan Wu <qx748086@dal.ca>
2023-03-30 01:04:03 -0300

    EspressoTest class created for Visualization2

---
+++ app/src/androidTest/java/com/example/group17project/VisualizationEspressoTest.java
@@ -0,0 +1,62 @@
+package com.example.group17project;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.junit.Assert.assertEquals;
+
+
+import android.content.Context;
+
+import androidx.test.espresso.ViewAction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.group17project.Homepages.HomepageActivity;
+import com.example.group17project.utils.model.Product;
+import com.example.group17project.utils.repository.ProductRepository;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+
+public class VisualizationEspressoTest {
+    @Rule
+    public ActivityScenarioRule<HomepageActivity> myRule = new ActivityScenarioRule<>(HomepageActivity.class);
+
+    @Before
+    public void setUp() throws Exception{
+    }
+
+    @After
+    public void tearDown() throws Exception{
+    }
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.group17project", appContext.getPackageName());
+    }
+
+}
