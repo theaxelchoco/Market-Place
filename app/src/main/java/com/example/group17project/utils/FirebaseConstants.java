a4b36ed327863c9e0106ce059d1e90ff3f023f04
Omar Dahleh <om885308@dal.ca>
2023-04-03 14:51:09 -0300

    initial setup, just have to change username to email and should be working

---
+++ app/src/main/java/com/example/group17project/utils/FirebaseConstants.java
@@ -0,0 +1,12 @@
+package com.example.group17project.utils;
+
+public class FirebaseConstants {
+
+    public static final String FIREBASE_URL = "https://w23-csci3130-group-17-default-rtdb.firebaseio.com/";
+    public static final String USERS_COLLECTION = "users";
+    public static final String CHAT_COLLECTION = "chat";
+
+    private FirebaseConstants() {
+        // Required private constructor
+    }
+}
